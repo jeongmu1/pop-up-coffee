73fdd03d40bf6cdb61452dbb8114bccf0aa85a0f
kang815595@gmail.com <Kang815595@gmail.com>
2023-12-05 14:15:33 +0900

    feat: Base 엔티티, BaseTime 엔티티 생성

---
+++ src/main/java/com/db8/popupcoffee/global/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.db8.popupcoffee.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
