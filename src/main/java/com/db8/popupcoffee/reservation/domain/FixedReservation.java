958dec908b06176318fc9a8466c47a2127990b96
Kang <Kang815595@gmail.com>
2023-12-06 09:24:06 +0900

    feat: 확정 예약 엔티티 생성

---
+++ src/main/java/com/db8/popupcoffee/reservation/domain/FixedReservation.java
@@ -0,0 +1,53 @@
+package com.db8.popupcoffee.reservation.domain;
+
+import com.db8.popupcoffee.contract.domain.MerchantContract;
+import com.db8.popupcoffee.global.domain.BaseTimeEntity;
+import com.db8.popupcoffee.rental.domain.SpaceRentalAgreement;
+import com.db8.popupcoffee.space.domain.Space;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FixedReservation extends BaseTimeEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private MerchantContract merchantContract;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private SpaceRentalAgreement spaceRentalAgreement;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FixedReservationStatus status;
+
+    @Column(nullable = false)
+    private LocalDate startingDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    private long paymentAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Space temporalSpace;
+
+    @Column(nullable = false)
+    private boolean fromFlexibleReservation;
+}
---
+++ src/main/java/com/db8/popupcoffee/reservation/domain/FixedReservationStatus.java
@@ -0,0 +1,10 @@
+package com.db8.popupcoffee.reservation.domain;
+
+public enum FixedReservationStatus {
+
+    FIXED,
+    SPACE_FIXED,
+    SPACE_TEMPORARY_FIXED,
+    SPACE_AWAITING,
+
+}
