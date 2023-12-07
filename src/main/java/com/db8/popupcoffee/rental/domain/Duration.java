ec4c126686777ff31c3b534ca98ffa17f888ffec
Kang <Kang815595@gmail.com>
2023-12-07 22:37:40 +0900

    fix: 공간 계약의 대여 기간 컬럼 추가

---
+++ src/main/java/com/db8/popupcoffee/rental/domain/Duration.java
@@ -0,0 +1,20 @@
+package com.db8.popupcoffee.rental.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Duration {
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+}
