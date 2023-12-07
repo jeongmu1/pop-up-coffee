64236018ef2eac0af5730bdfd2428b13f93ed6c3
Kang <Kang815595@gmail.com>
2023-12-07 16:42:34 +0900

    feat: 활성화된 업체 계약 조회 구현

--- src/main/java/com/db8/popupcoffee/contract/repository/MerchantContractRepository.java
+++ src/main/java/com/db8/popupcoffee/contract/repository/MerchantContractRepository.java
@@ -1,8 +1,12 @@
 package com.db8.popupcoffee.contract.repository;
 
 import com.db8.popupcoffee.contract.domain.MerchantContract;
+import com.db8.popupcoffee.merchant.domain.Merchant;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MerchantContractRepository extends CrudRepository<MerchantContract, Long> {
 
+    Optional<MerchantContract> findByMerchantAndExpireAtAfter(Merchant merchant, LocalDate yesterday);
 }
--- src/main/java/com/db8/popupcoffee/contract/service/ContractService.java
+++ src/main/java/com/db8/popupcoffee/contract/service/ContractService.java
@@ -1,9 +1,11 @@
 package com.db8.popupcoffee.contract.service;
 
 import com.db8.popupcoffee.contract.controller.dto.request.CreateContractRequest;
+import com.db8.popupcoffee.contract.domain.MerchantContract;
 import com.db8.popupcoffee.contract.repository.MerchantContractRepository;
 import com.db8.popupcoffee.merchant.domain.Merchant;
 import com.db8.popupcoffee.merchant.repository.MerchantRepository;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +20,10 @@ public class ContractService {
         Merchant merchant = merchantRepository.findById(form.merchantId())
             .orElseThrow();
         merchantContractRepository.save(form.toEntity(merchant));
+    }
+
+    public MerchantContract findActivatedContract(Merchant merchant) {
+        return merchantContractRepository.findByMerchantAndExpireAtAfter(merchant,
+            LocalDate.now().minusDays(1L)).orElseThrow();
     }
 }
