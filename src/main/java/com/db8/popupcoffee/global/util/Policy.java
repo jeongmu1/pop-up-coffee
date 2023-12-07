278a4537951a28f5aa17257819a5e692906b5e9d
Kang <Kang815595@gmail.com>
2023-12-07 20:17:51 +0900

    feat: 등급 열거형 클래스 생성 및 점수 계산 메소드 구현

--- src/main/java/com/db8/popupcoffee/global/util/Policy.java
+++ src/main/java/com/db8/popupcoffee/global/util/Policy.java
@@ -12,7 +12,7 @@ public class Policy {
     public static final long LOW_SEASON_DISCOUNT = 25000;
     public static final int FIRST_RENTAL_SCORE_CHANGES = 200;
     public static final int SCORE_CHANGES_PER_DAY = 5;
-    public static final int SCORE_CHANGES_PER_10K = 2; // 매출 만원 당 등급점수
+    public static final int REVENUE_FOR_ONE_SCORE = 5000; // 매출 만원 당 등급점수
     public static final int GREEN_MIN_SCORE = 250;
     public static final int PURPLE_MIN_SCORE = 500;
     public static final int VIP_MIN_SCORE = 1000;
---
+++ src/main/java/com/db8/popupcoffee/merchant/domain/Grade.java
@@ -0,0 +1,66 @@
+package com.db8.popupcoffee.merchant.domain;
+
+import static com.db8.popupcoffee.global.util.Policy.*;
+
+public enum Grade {
+    WHITE(0),
+    GREEN(GREEN_MIN_SCORE),
+    PURPLE(PURPLE_MIN_SCORE),
+    VIP(VIP_MIN_SCORE);
+
+    private final int minimumScore;
+
+    Grade(int minimumScore) {
+        this.minimumScore = minimumScore;
+    }
+
+    public static Grade from(int gradeScore) {
+        if (gradeScore > VIP.minimumScore) {
+            return VIP;
+        }
+        if (gradeScore > PURPLE.minimumScore) {
+            return PURPLE;
+        }
+        if (gradeScore > GREEN.minimumScore) {
+            return GREEN;
+        }
+        return WHITE;
+    }
+
+    public Grade nextGrade() {
+        return switch (this) {
+            case VIP -> null;
+            case PURPLE -> VIP;
+            case GREEN -> PURPLE;
+            case WHITE -> GREEN;
+        };
+    }
+
+    public int scoreForNextGrade(int gradeScore) {
+        return switch (nextGrade()) {
+            case VIP -> 0;
+            case PURPLE -> VIP.minimumScore - gradeScore;
+            case GREEN -> GREEN.minimumScore - gradeScore;
+            case WHITE -> throw new IllegalArgumentException("다음 등급이 WHITE 가 될 수 없습니다.");
+        };
+    }
+
+    public long getDaysForNextGrade(int gradeScore) {
+        if (this == VIP) {
+            return 0;
+        }
+        int rest = scoreForNextGrade(gradeScore) % SCORE_CHANGES_PER_DAY;
+
+        if (rest > 0) {
+            return scoreForNextGrade(gradeScore) / SCORE_CHANGES_PER_DAY + 1;
+        }
+        return scoreForNextGrade(gradeScore) / SCORE_CHANGES_PER_DAY;
+    }
+
+    public long revenueForNextGrade(int gradeScore) {
+        if (this == VIP) {
+            return 0;
+        }
+        return (long) scoreForNextGrade(gradeScore) * REVENUE_FOR_ONE_SCORE;
+    }
+}
