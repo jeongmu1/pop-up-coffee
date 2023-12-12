1ed2df7ae1a78dcfc57f663e6a079491013ed74b
nowgnodeel <dawn990323@gmail.com>
2023-12-12 21:28:01 +0900

    fix: 업체 계약 생성 부분 변경

--- src/main/java/com/db8/popupcoffee/contract/repository/MerchantContractRepository.java
+++ src/main/java/com/db8/popupcoffee/contract/repository/MerchantContractRepository.java
@@ -2,11 +2,14 @@ package com.db8.popupcoffee.contract.repository;
 
 import com.db8.popupcoffee.contract.domain.MerchantContract;
 import com.db8.popupcoffee.merchant.domain.Merchant;
+import org.springframework.data.repository.CrudRepository;
+
 import java.time.LocalDate;
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
 
 public interface MerchantContractRepository extends CrudRepository<MerchantContract, Long> {
 
     Optional<MerchantContract> findByMerchantAndExpireAtAfter(Merchant merchant, LocalDate yesterday);
+
+    boolean existsByMerchant(Merchant merchant);
 }
--- src/main/java/com/db8/popupcoffee/merchant/repository/MerchantRepository.java
+++ src/main/java/com/db8/popupcoffee/merchant/repository/MerchantRepository.java
@@ -2,8 +2,9 @@ package com.db8.popupcoffee.merchant.repository;
 
 import com.db8.popupcoffee.global.domain.AuthenticationInfo;
 import com.db8.popupcoffee.merchant.domain.Merchant;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 public interface MerchantRepository extends CrudRepository<Merchant, Long> {
 
