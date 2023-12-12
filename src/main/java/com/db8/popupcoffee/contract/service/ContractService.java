0c1f2a92931ed9a8d173e1a311fe80dcafd48cb1
nowgnodeel <dawn990323@gmail.com>
2023-12-12 21:28:09 +0900

    fix: 업체 계약 생성 부분 변경

--- src/main/java/com/db8/popupcoffee/contract/service/ContractService.java
+++ src/main/java/com/db8/popupcoffee/contract/service/ContractService.java
@@ -5,9 +5,14 @@ import com.db8.popupcoffee.contract.domain.MerchantContract;
 import com.db8.popupcoffee.contract.repository.MerchantContractRepository;
 import com.db8.popupcoffee.merchant.domain.Merchant;
 import com.db8.popupcoffee.merchant.repository.MerchantRepository;
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +30,20 @@ public class ContractService {
     public MerchantContract findActivatedContract(Merchant merchant) {
         return merchantContractRepository.findByMerchantAndExpireAtAfter(merchant,
             LocalDate.now().minusDays(1L)).orElseThrow();
+    }
+
+    public List<Merchant> findMerchantsWithoutContracts() {
+        Iterable<Merchant> allMerchantsIterable = merchantRepository.findAll();
+        List<Merchant> allMerchants = StreamSupport.stream(allMerchantsIterable.spliterator(), false)
+            .collect(Collectors.toList());
+        List<Merchant> merchantsWithoutContracts = new ArrayList<>();
+
+        for (Merchant merchant : allMerchants) {
+            if (!merchantContractRepository.existsByMerchant(merchant)) {
+                merchantsWithoutContracts.add(merchant);
+            }
+        }
+
+        return merchantsWithoutContracts;
     }
 }
