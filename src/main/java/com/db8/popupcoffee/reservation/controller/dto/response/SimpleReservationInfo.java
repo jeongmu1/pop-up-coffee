faab758a3408d83281498c592cf031a104190f38
Kang <Kang815595@gmail.com>
2023-12-13 12:57:27 +0900

    feat: 공간 배정설정 구현

--- src/main/java/com/db8/popupcoffee/reservation/controller/dto/response/SimpleReservationInfo.java
+++ src/main/java/com/db8/popupcoffee/reservation/controller/dto/response/SimpleReservationInfo.java
@@ -1,9 +1,11 @@
 package com.db8.popupcoffee.reservation.controller.dto.response;
 
+import com.db8.popupcoffee.reservation.domain.DesiredDate;
 import com.db8.popupcoffee.reservation.domain.FixedReservation;
 import com.db8.popupcoffee.reservation.domain.FlexibleReservation;
 import com.db8.popupcoffee.reservation.domain.FlexibleReservationStatus;
 import java.time.LocalDate;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
@@ -15,7 +17,8 @@ public record SimpleReservationInfo(
     // 유동 관련
     boolean fromFlexible,
     Boolean fixedDate, // null 시 Fixed
-    Long rentalDuration // null 시 무관n
+    Long rentalDuration, // null 시 무관n
+    List<LocalDate> desiredDates
 ) {
 
     public static SimpleReservationInfo from(FlexibleReservation flexibleReservation) {
@@ -28,6 +31,7 @@ public record SimpleReservationInfo(
             .fixedDate(status.equals(FlexibleReservationStatus.SPACE_FIXED) || status.equals(
                 FlexibleReservationStatus.RESERVATION_FIXED))
             .rentalDuration(flexibleReservation.getDuration())
+            .desiredDates(flexibleReservation.getDesiredDates().stream().map(DesiredDate::getDate).toList())
             .build();
     }
 
